64647381ea6622ac60cc4683e6ec7b7c9c7db036
Felipe Carvalho <feliped3carvalho@gmail.com>
2023-09-14 01:56:59 -0300

    feat: create a new todo

--- TodoList/Models/ToDoModel.swift
+++ TodoList/Models/ToDoModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct TodoModel: Codable, Identifiable {
+    let id: String
+    let title: String
+    let dueDate: TimeInterval
+    let createdDate: TimeInterval
+    var isDone: Bool
+    
+    //should use mutating in a struct
+    mutating func setDone(_ state: Bool) {
+        isDone = state
+    }
+}
