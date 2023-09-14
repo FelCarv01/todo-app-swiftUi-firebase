86a2b3d4c65a37dabde0fbd3f6e7d0448d22b9e2
Felipe Carvalho <feliped3carvalho@gmail.com>
2023-09-14 04:48:50 -0300

    feat: profile screan

--- TodoList/Views/ProfileView.swift
+++ TodoList/Views/ProfileView.swift
@@ -13,9 +13,52 @@ struct ProfileView: View {
     var body: some View {
         NavigationView{
             VStack{
-                
+                if let user = viewModel.user {
+                    profile(user: user)
+                    
+                } else {
+                    Text("Carregando o perfil . . . ")
+                }
             }.navigationTitle("Perfil")
+            
+        }.onAppear{
+            viewModel.fetchUser()
         }
+    }
+    
+    @ViewBuilder
+    func profile(user: UserModel) -> some View {
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(Color.blue)
+            .frame(width: 125).padding()
+        
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Nome: ").bold()
+                Text(user.userName)
+            }
+            .padding()
+            HStack {
+                Text("Email: ").bold()
+                Text(user.email)
+            }
+            .padding()
+            HStack {
+                Text("Membro desde: ").bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding()
+        }
+        
+        Spacer()
+        
+        TLButton(title: "Sair", backgroundColor: Color.pink) {
+            viewModel.logOut()
+        }.frame(maxWidth: 140, maxHeight: 56)
+        
+        Spacer()
     }
 }
 
